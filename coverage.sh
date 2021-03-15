827bbeb44209de6ec4534ec61348e310630b7022
Justin thompson <justinthompson@Justins-MacBook-Pro.local>
2021-03-15 13:53:43 -0600

    try this

--- coverage.sh
+++ coverage.sh
@@ -2,8 +2,6 @@
 
 set -e -u
 
-go test ./... -coverprofile cover.out -covermode atomic
-
 perc=`go tool cover -func=coverage-check.out | tail -n 1 | sed -Ee 's!^[^[:digit:]]+([[:digit:]]+(\.[[:digit:]]+))%$!\1!'`
 res=`echo "$perc >= 80.0" | bc`
 test "$res" -eq 1 && exit 0
